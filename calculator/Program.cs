1383cbe02427f53179f4430b913832eff8ec76ae
gen-la <147514418+gen-la@users.noreply.github.com>
2023-12-09 19:12:43 +0100

    Update Program.cs

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace calculator; 
 
-internal class Program
+class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
         //Pseudo kod
         // Välkomnande meddelande
@@ -12,6 +16,78 @@ internal class Program
         // En lista för att spara historik för räkningar
         // Användaren matar in tal och matematiska operation
         // OBS! Användaren måsta mata in ett tal för att kunna ta sig vidare i programmet!
+        Console.WriteLine("Skriv in det första talet: ");
+        double firstNum = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Skriv in det andra talet: ");
+        double secondNum = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine($"Välj en operator (+ - / *): ");
+        string val = Console.ReadLine();
+
+        switch (val)
+        {
+            case "+":
+                Console.WriteLine(firstNum + secondNum);
+                break;
+            case "-":
+                Console.WriteLine(firstNum - secondNum);
+                break;
+            case "/":
+                Console.WriteLine(firstNum / secondNum);
+                break;
+            case "*":
+                Console.WriteLine(firstNum * secondNum);
+                break;
+            default:
+                Console.Write("Felaktig inmatning. Försök igen.\n");
+                Main();
+                break;
+        }
+
+
+        /*if (val == "+" || val == "-" || val == "/" || val == "*")
+        {
+            switch (val)
+            {
+                case "+":
+                    Console.WriteLine(firstNum + secondNum);
+                    break;
+                case "-":
+                    Console.WriteLine(firstNum - secondNum);
+                    break;
+                case "/":
+                    Console.WriteLine(firstNum / secondNum);
+                    break;
+                case "*":
+                    Console.WriteLine(firstNum * secondNum);
+                    break;
+                default:
+                    Console.Write("Felaktigt menyval, försök igen.");
+                    break;
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Felaktig inmatning. Välj en operator (+ - / *): ");
+            val = Console.ReadLine();
+            switch (val)
+            {
+                case "+":
+                    Console.WriteLine(firstNum + secondNum);
+                    break;
+                case "-":
+                    Console.WriteLine(firstNum - secondNum);
+                    break;
+                case "/":
+                    Console.WriteLine(firstNum / secondNum);
+                    break;
+                case "*":
+                    Console.WriteLine(firstNum * secondNum);
+                    break;
+                default:
+                    Console.Write("Felaktigt menyval, försök igen.");
+                    break;
+            }
+        }*/
         // Ifall användaren skulle dela med 0 visa Ogiltig inmatning!
         // Lägga resultat till listan
         // Visa resultat
