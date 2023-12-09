1e2ffc33eff26af4238b70773c90ce190a28f6b5
gen-la <147514418+gen-la@users.noreply.github.com>
2023-12-09 18:06:05 +0100

    Add project files.

---
+++ calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace calculator; 
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //Pseudo kod
+        // Välkomnande meddelande
+        Console.WriteLine($"Välkommen till miniräknaren. Du kan utföra beräkningar med operatorerna + - / och *");
+        // En lista för att spara historik för räkningar
+        // Användaren matar in tal och matematiska operation
+        // OBS! Användaren måsta mata in ett tal för att kunna ta sig vidare i programmet!
+        // Ifall användaren skulle dela med 0 visa Ogiltig inmatning!
+        // Lägga resultat till listan
+        // Visa resultat
+        // Fråga användaren om den vill visa tidigare resultat.
+        // Visa tidigare resultat
+        // Fråga användaren om den vill avsluta eller fortsätta.
+
+
+
+    }
+}
