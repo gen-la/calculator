a85c2b188e02d86f312f2a4850411f46a232e635
gen-la <147514418+gen-la@users.noreply.github.com>
2023-12-11 14:47:02 +0100

    liten uppdatering
    nu kollar programmet om användaren försöker dividera med 0 och ber om en siffra större än 0

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,6 +15,7 @@ class Program
         double secondNum = 0;
         int menyVal = 0;
         // En lista för att spara historik för räkningar
+        // Visa tidigare resultat
 
         // Välkomnande meddelande
         Console.WriteLine($"Välkommen till miniräknaren. Du kan utföra beräkningar med operatorerna + - / och *\n");
@@ -53,8 +55,55 @@ class Program
         chooseOperator:
             Console.WriteLine($"Välj en operator (+ - / *): ");
             string valOperator = Console.ReadLine();
+            if ( valOperator=="+" || valOperator=="-" || valOperator=="/" || valOperator=="*")
+            {
+                switch (valOperator)
+                {
+                    case "+":
+                        Console.WriteLine(firstNum + secondNum);
+                        goto fortsattLRavslut;
+                        break;
+                    case "-":
+                        Console.WriteLine(firstNum - secondNum);
+                        goto fortsattLRavslut;
+                        break;
+                    case "/":
+                        if (firstNum <= 0 || secondNum <= 0)
+                        {
+                            goto default;
+                        }
+                        else 
+                        {
+                            Console.WriteLine(firstNum / secondNum);
+                            goto fortsattLRavslut;
+                        }
+                        break;
+                    case "*":
+                        Console.WriteLine(firstNum * secondNum);
+                        goto fortsattLRavslut;
+                        break;
+                    default:    // Ifall användaren skulle dela med 0 visa Ogiltig inmatning!
+                        while (firstNum <= 0)
+                        {
+                            Console.WriteLine("Division med 0 går inte, skriv ett tal större än 0:");
+                            firstNum = Convert.ToDouble(Console.ReadLine());
+                        }
+                        while (secondNum <= 0)
+                        {
+                            Console.WriteLine("Division med 0 går inte, skriv ett tal större än 0:");
+                            secondNum = Convert.ToDouble(Console.ReadLine());
+                        }
+                        goto case "/";
+                        break;
+                }
+            }
+            else 
+            {
+                Console.Write("Felaktig inmatning. Försök igen.\n");
+                goto chooseOperator;
+            }
 
-        switch (valOperator)
+        /*switch (valOperator)
         {
             case "+":
                 Console.WriteLine(firstNum + secondNum);
@@ -73,11 +122,11 @@ class Program
                 goto fortsattLRavslut;
                 break;
             default:
-                Console.Write("Felaktig inmatning. Försök igen.\n");
+                //Console.Write("Felaktig inmatning. Försök igen.\n");
                 //Main();
-                goto chooseOperator;
+                //goto chooseOperator;
                 break;
-        }
+        }*/
 
         // Fråga användaren om den vill avsluta eller fortsätta.
         fortsattLRavslut:
@@ -147,14 +196,8 @@ class Program
                     break;
             }
         }*/
-        // Ifall användaren skulle dela med 0 visa Ogiltig inmatning!
         // Lägga resultat till listan
         // Visa resultat
         // Fråga användaren om den vill visa tidigare resultat.
-        // Visa tidigare resultat
-        
-
-
-
     }
 }
